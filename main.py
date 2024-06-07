fe31c1c25eb72f563df8bd31d73dc9d6ec67fb0d
gaulerie <166921833+gaulerie@users.noreply.github.com>
2024-06-07 22:27:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import tweepy
 import pendulum
 import uuid
+import requests
 
 # Configurer l'API v1.1 pour le téléchargement de médias
 consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
@@ -18,71 +19,81 @@ api_v1 = tweepy.API(auth)
 bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
 client_v2 = tweepy.Client(bearer_token, consumer_key, consumer_secret_key, access_token, access_token_secret)
 
-now = pendulum.now("Europe/Paris")
-with open("tweet.json") as file:
-    tweets = json.load(file)
-    for time in list(tweets):
-        tweet_time = pendulum.parse(time, tz="Europe/Paris")
-        if tweet_time < now:
-            if len(tweets[time].keys()) == 1:
-                for tweet_text, image_path in tweets[time].items():
-                    unique_tweet_text = f"{tweet_text} - {uuid.uuid4()}"
+# Récupérer les données depuis le déploiement Google Apps Script
+response = requests.get("https://script.google.com/macros/s/AKfycbwOKMaljj2zYgT09EEz-eBLMYbs6DUd6514_MxlxNDWZNZoikJGlq3yaDuqkLqRx8R9/exec")
+if response.status_code == 200:
+    tweets = response.json()
+else:
+    print(f"Erreur lors de la récupération des données : {response.status_code}")
+    tweets = {}
+
+# Initialiser le fuseau horaire
+now = pendulum.now("Asia/Dhaka")
+
+# Publier les tweets
+for time in list(tweets):
+    tweet_time = pendulum.parse(time, tz="Europe/Paris")
+    if tweet_time < now:
+        if len(tweets[time].keys()) == 1:
+            for tweet_text, image_path in tweets[time].items():
+                unique_tweet_text = f"{tweet_text} - {uuid.uuid4()}"
+                if tweet_text and image_path:
+                    media = api_v1.media_upload(image_path)
+                    client_v2.create_tweet(text=unique_tweet_text, media_ids=[media.media_id_string])
+                    os.remove(image_path)
+                elif tweet_text:
+                    client_v2.create_tweet(text=unique_tweet_text)
+                elif image_path:
+                    media = api_v1.media_upload(image_path)
+                    client_v2.create_tweet(media_ids=[media.media_id_string])
+                    os.remove(image_path)
+        else:
+            prev_tweet_id = None
+            for index, (tweet_text, image_path) in enumerate(tweets[time].items()):
+                unique_tweet_text = f"{tweet_text} - {uuid.uuid4()}"
+                if index == 0:
                     if tweet_text and image_path:
                         media = api_v1.media_upload(image_path)
-                        client_v2.create_tweet(text=unique_tweet_text, media_ids=[media.media_id_string])
-                        os.remove(image_path)
-                    elif tweet_text:
-                        client_v2.create_tweet(text=unique_tweet_text)
-                    elif image_path:
-                        media = api_v1.media_upload(image_path)
-                        client_v2.create_tweet(media_ids=[media.media_id_string])
-                        os.remove(image_path)
-            else:
-                prev_tweet_id = None
-                for index, (tweet_text, image_path) in enumerate(tweets[time].items()):
-                    unique_tweet_text = f"{tweet_text} - {uuid.uuid4()}"
-                    if index == 0:
-                        if tweet_text and image_path:
-                            media = api_v1.media_upload(image_path)
-                            prev_tweet = client_v2.create_tweet(
-                                text=unique_tweet_text, media_ids=[media.media_id_string]
-                            )
-                            prev_tweet_id = prev_tweet.data["id"]
-                            os.remove(image_path)
-                        elif tweet_text:
-                            prev_tweet = client_v2.create_tweet(text=unique_tweet_text)
-                            prev_tweet_id = prev_tweet.data["id"]
-                        elif image_path:
-                            media = api_v1.media_upload(image_path)
-                            prev_tweet = client_v2.create_tweet(
-                                media_ids=[media.media_id_string]
-                            )
-                            prev_tweet_id = prev_tweet.data["id"]
-                            os.remove(image_path)
-                    elif tweet_text and image_path:
-                        media = api_v1.media_upload(image_path)
                         prev_tweet = client_v2.create_tweet(
-                            text=unique_tweet_text,
-                            media_ids=[media.media_id_string],
-                            in_reply_to_tweet_id=prev_tweet_id,
+                            text=unique_tweet_text, media_ids=[media.media_id_string]
                         )
                         prev_tweet_id = prev_tweet.data["id"]
                         os.remove(image_path)
                     elif tweet_text:
-                        prev_tweet = client_v2.create_tweet(
-                            text=unique_tweet_text,
-                            in_reply_to_tweet_id=prev_tweet_id,
-                        )
+                        prev_tweet = client_v2.create_tweet(text=unique_tweet_text)
                         prev_tweet_id = prev_tweet.data["id"]
                     elif image_path:
                         media = api_v1.media_upload(image_path)
                         prev_tweet = client_v2.create_tweet(
-                            media_ids=[media.media_id_string],
-                            in_reply_to_tweet_id=prev_tweet_id,
+                            media_ids=[media.media_id_string]
                         )
                         prev_tweet_id = prev_tweet.data["id"]
                         os.remove(image_path)
-            tweets.pop(time)
+                elif tweet_text and image_path:
+                    media = api_v1.media_upload(image_path)
+                    prev_tweet = client_v2.create_tweet(
+                        text=unique_tweet_text,
+                        media_ids=[media.media_id_string],
+                        in_reply_to_tweet_id=prev_tweet_id,
+                    )
+                    prev_tweet_id = prev_tweet.data["id"]
+                    os.remove(image_path)
+                elif tweet_text:
+                    prev_tweet = client_v2.create_tweet(
+                        text=unique_tweet_text,
+                        in_reply_to_tweet_id=prev_tweet_id,
+                    )
+                    prev_tweet_id = prev_tweet.data["id"]
+                elif image_path:
+                    media = api_v1.media_upload(image_path)
+                    prev_tweet = client_v2.create_tweet(
+                        media_ids=[media.media_id_string],
+                        in_reply_to_tweet_id=prev_tweet_id,
+                    )
+                    prev_tweet_id = prev_tweet.data["id"]
+                    os.remove(image_path)
+        tweets.pop(time)
 
+# Enregistrer les tweets restants dans le fichier JSON
 with open("tweet.json", "w") as file:
-    file.write(json.dumps(tweets))
+    json.dump(tweets, file)
