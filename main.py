cc8c1298b7b21f7779925ab713421b7d3428fe9e
gaulerie <166921833+gaulerie@users.noreply.github.com>
2024-06-07 21:56:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import json
 import tweepy
 import pendulum
 
+# Configurer l'API v1.1 pour le téléchargement de médias
 consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
 consumer_secret_key = os.environ.get("TWITTER_CONSUMER_SECRET_KEY")
 access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
 
-client = tweepy.Client(bearer_token, consumer_key, consumer_secret_key, access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+api_v1 = tweepy.API(auth)
+
+# Configurer l'API v2 pour la publication de tweets
+bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
+client_v2 = tweepy.Client(bearer_token, consumer_key, consumer_secret_key, access_token, access_token_secret)
 
 now = pendulum.now("Asia/Dhaka")
 with open("tweet.json") as file:
@@ -20,55 +26,55 @@ with open("tweet.json") as file:
             if len(tweets[time].keys()) == 1:
                 for tweet_text, image_path in tweets[time].items():
                     if tweet_text and image_path:
-                        media = client.media_upload(image_path)
-                        client.create_tweet(text=tweet_text, media_ids=[media.media_id])
+                        media = api_v1.media_upload(image_path)
+                        client_v2.create_tweet(text=tweet_text, media_ids=[media.media_id_string])
                         os.remove(image_path)
                     elif tweet_text:
-                        client.create_tweet(text=tweet_text)
+                        client_v2.create_tweet(text=tweet_text)
                     elif image_path:
-                        media = client.media_upload(image_path)
-                        client.create_tweet(media_ids=[media.media_id])
+                        media = api_v1.media_upload(image_path)
+                        client_v2.create_tweet(media_ids=[media.media_id_string])
                         os.remove(image_path)
             else:
                 prev_tweet_id = None
                 for index, (tweet_text, image_path) in enumerate(tweets[time].items()):
                     if index == 0:
                         if tweet_text and image_path:
-                            media = client.media_upload(image_path)
-                            prev_tweet = client.create_tweet(
-                                text=tweet_text, media_ids=[media.media_id]
+                            media = api_v1.media_upload(image_path)
+                            prev_tweet = client_v2.create_tweet(
+                                text=tweet_text, media_ids=[media.media_id_string]
                             )
                             prev_tweet_id = prev_tweet.data["id"]
                             os.remove(image_path)
                         elif tweet_text:
-                            prev_tweet = client.create_tweet(text=tweet_text)
+                            prev_tweet = client_v2.create_tweet(text=tweet_text)
                             prev_tweet_id = prev_tweet.data["id"]
                         elif image_path:
-                            media = client.media_upload(image_path)
-                            prev_tweet = client.create_tweet(
-                                media_ids=[media.media_id]
+                            media = api_v1.media_upload(image_path)
+                            prev_tweet = client_v2.create_tweet(
+                                media_ids=[media.media_id_string]
                             )
                             prev_tweet_id = prev_tweet.data["id"]
                             os.remove(image_path)
                     elif tweet_text and image_path:
-                        media = client.media_upload(image_path)
-                        prev_tweet = client.create_tweet(
+                        media = api_v1.media_upload(image_path)
+                        prev_tweet = client_v2.create_tweet(
                             text=tweet_text,
-                            media_ids=[media.media_id],
+                            media_ids=[media.media_id_string],
                             in_reply_to_tweet_id=prev_tweet_id,
                         )
                         prev_tweet_id = prev_tweet.data["id"]
                         os.remove(image_path)
                     elif tweet_text:
-                        prev_tweet = client.create_tweet(
+                        prev_tweet = client_v2.create_tweet(
                             text=tweet_text,
                             in_reply_to_tweet_id=prev_tweet_id,
                         )
                         prev_tweet_id = prev_tweet.data["id"]
                     elif image_path:
-                        media = client.media_upload(image_path)
-                        prev_tweet = client.create_tweet(
-                            media_ids=[media.media_id],
+                        media = api_v1.media_upload(image_path)
+                        prev_tweet = client_v2.create_tweet(
+                            media_ids=[media.media_id_string],
                             in_reply_to_tweet_id=prev_tweet_id,
                         )
                         prev_tweet_id = prev_tweet.data["id"]
